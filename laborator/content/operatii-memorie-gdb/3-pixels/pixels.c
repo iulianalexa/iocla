4ff48d6adbaad9c82bce286add34d7027b1f6209
Iulian Alexa <116089722+iulianalexa@users.noreply.github.com>
2023-03-20 02:48:43 +0200

    lab2: ex3: Added solutions for exercise 3
    Signed-off-by: Iulian Alexa <116089722+iulianalexa@users.noreply.github.com>

--- laborator/content/operatii-memorie-gdb/3-pixels/pixels.c
+++ laborator/content/operatii-memorie-gdb/3-pixels/pixels.c
@@ -7,6 +7,8 @@
 #include <time.h>
 #include "pixel.h"
 
+#define GET_PIXEL(a, i ,j) (*(*(a + i) + j))
+
 /*
  * TODO a
  * Functia primeste ca parametru o imagine si intoarce imaginea rasturnata.
@@ -15,7 +17,14 @@
  * linia n - 1, etc.
  */
 
-void reversePic(Picture *pic);
+void reversePic(Picture *pic) {
+	int i, n = (*pic).height;
+	for (i = 0; i < n / 2; i++) {
+		Pixel *aux = *((*pic).pix_array + i);
+		*((*pic).pix_array + i) = *((*pic).pix_array + (n - i - 1));
+		*((*pic).pix_array + (n - i - 1)) = aux;
+	}
+}
 
 /*
  * TODO b
@@ -27,7 +36,19 @@ void reversePic(Picture *pic);
  * p.b = 0.11 * p.b;
  */
 
-void colorToGray(Picture *pic);
+void colorToGray(Picture *pic) {
+	int i, j;
+	Pixel p;
+	for (i = 0; i < (*pic).height; i++) {
+		for (j = 0; j < (*pic).width; j++) {
+			p = (*pic).pix_array[i][j];
+			p.R = 0.3 * p.R;
+			p.G = 0.59 * p.G;
+			p.B = 0.11 * p.B;
+			(*pic).pix_array[i][j] = p;
+		}
+	}
+}
 
 /*
  * Structura unui pixel, cea a unei imagini, precum si generarea acestora
@@ -49,8 +70,18 @@ int main(void)
 
 	printPicture(pic);
 
+	printf("-----------\n");
+	reversePic(pic);
+	printPicture(pic);
+
+	printf("-----------\n");
+	colorToGray(pic);
+	printPicture(pic);
+
 	freePicture(&pic);
 	freePixelArray(&pix_array, height, width);
+	
+	
 
 	return 0;
 }
