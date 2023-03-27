27e8949e39e933025ed92899d5978d0376c5b812
Iulian Alexa <116089722+iulianalexa@users.noreply.github.com>
2023-03-27 04:49:26 +0300

    lab3: bonus: Added solutions for bonus exercise.
    Signed-off-by: Iulian Alexa <116089722+iulianalexa@users.noreply.github.com>

--- laborator/content/etapele-compilarii/bonus-c-python/python_modules/my_module.py
+++ laborator/content/etapele-compilarii/bonus-c-python/python_modules/my_module.py
@@ -1,6 +1,15 @@
 def say(something='Something default'):
     print(something)
 
+def subsir(str1, str2):
+	if str2 in str1:
+		i = 0
+		while not str1[i:].startswith(str2):
+			i = i + 1
+		return i
+	
+	return -1
+
 # TODO
 # Definiți o nouă funcție care întoarce poziția primei
 # apariții a celui de-al doilea șir în cadrul primului,
